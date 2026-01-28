b1ebe1efd9018b31f4530031a7ddd9cda0e8e1e4
XineohpM <150469605+XineohpM@users.noreply.github.com>
2026-01-27 21:04:22 -0800

    Added usage examples for push.py.

--- util/push.py
+++ util/push.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python3
+"""
+push.py
+
+Append absolute paths of HDF5 files into a stack file (one path per line).
+This script expands glob patterns itself (via glob()), so you should quote patterns.
+
+USAGE
+-----
+python3 push.py STACKFILE PATTERN [PATTERN2 ...]
+python3 push.py STACKFILE a.h5 b.h5 ...
+
+EXAMPLES
+--------
+# (1) From a run directory that contains T_* subfolders:
+cd /scratch/users/phoenixm/dqmc_runs/U-2_n6x6_C
+python3 /home/users/phoenixm/dqmc-dev/util/push.py stack_energy 'T_*/*.h5'
+
+# (2) Append only a subset (e.g. a prefix):
+python3 /home/users/phoenixm/dqmc-dev/util/push.py stack_energy 'T_0.1/C_U-2_*.h5' 'T_0.2/C_U-2_*.h5'
+
+# NOTE
+# - This script appends to STACKFILE. Re-running with the same PATTERN will duplicate entries.
+# - If you want a fresh stackfile, delete it first: rm -f stack_energy
+"""
+
 import os
 import sys
 from glob import glob
